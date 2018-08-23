23ec48f588ccd896fd9db16865e0b1f9619405fd
mjksend <mjksendonly@gmail.com>
2018-08-23 15:04:24 +0900

    pass administrator password= test

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    example "文字列を与えると60文字のハッシュがhash_passwordに入る" do
+      admin = Administrator.new
+      admin.password = "password"
+      expect(admin.hashed_password).to be_kind_of(String)
+      expect(admin.hashed_password.size).to eq(60)
+    end
+
+    example "passwordがnilならhased_passwordもnilになる" do
+      admin = Administrator.new(hashed_password: 'x')
+      admin.password = nil
+      expect(admin.hashed_password).to be_nil
+    end
+  end
 end
