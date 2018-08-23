f74de8ea7c039e38b912d0bb9402bcc3c782c9de
mjksend <mjksendonly@gmail.com>
2018-08-23 19:55:30 +0900

    done 10-3

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,14 @@ Rails.application.routes.draw do
   namespace :staff, path: '' do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [ :create, :destroy ]
+    resource :account, except: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
-    # get 'staff_members' => 'staff_members#index'
-    # get 'staff_members/:id' => 'staff_members#show'
-    # get 'staff_members/new' => 'staff_members#new'
-    # get 'staff_members/:id/edit' => 'staff_members#edit'
-    # post 'staff_members' => 'staff_members#create'
-    # patch 'staff_members/:id' => 'staff_members#update'
-    # delete 'staff_members/:id' => 'staff_members#destroy'
+    resource :session, only: [ :create, :destroy ]
     resources :staff_members
   end
 
