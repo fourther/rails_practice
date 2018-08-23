573a85bc5440643ffb617b49b2e99ad83c9ea58d
mjksend <mjksendonly@gmail.com>
2018-08-24 01:44:24 +0900

    done 12-1

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,6 +3,7 @@ module ErrorHandlers
 
     included do
         rescue_from Exception, with: :rescue500
+        rescue_from ActionController::ParameterMissing, with: :rescue400
         rescue_from ApplicationController::Forbidden, with: :rescue403
         rescue_from ApplicationController::IpAddressRejected, with: :rescue403
         rescue_from ActionController::RoutingError, with: :rescue404
@@ -11,6 +12,11 @@ module ErrorHandlers
 
 
   private
+  def rescue400(e)
+    @exception = e
+    render 'errors/bad_request', status: 400
+  end
+
   def rescue403(e)
     @exception = e
     render 'errors/forbidden', status: 403
