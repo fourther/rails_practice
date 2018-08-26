4d8bec204cf2b809d198162a3325fa9c29867822
mjksend <mjksendonly@gmail.com>
2018-08-27 01:39:21 +0900

    p383

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -2,6 +2,16 @@ class StaffMemberFormPresenter < FormPresenter
     def password_field_block(name, label_text, options = {})
         if object.new_record?
             super(name, label_text, options)
+        else
+            markup(:div, class: 'input-block') do |m|
+                m << decorated_label(name, label_text, options.merge(required: false))
+                m << password_field(name, options.merge(disabled: true))
+                m.button('変更する', type: 'button', id: 'enable-password-field')
+                m.button('変更しない', type: 'button', id: 'disable-password-field',
+                         style: 'display: none')
+                m << error_messages_for(name)
+            end
+
         end
     end
 
@@ -21,4 +31,5 @@ class StaffMemberFormPresenter < FormPresenter
             m << label(:suspended, 'アカウント停止')
         end
     end
+
 end
