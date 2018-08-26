124e39502eaed0afea608d82c2b086b025ff8182
mjksend <mjksendonly@gmail.com>
2018-08-26 11:17:20 +0900

    done p337

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -5,8 +5,16 @@ class Staff::ChangePasswordForm
                   :new_password_confirmation
     validates :new_password, presence: true, confirmation: true
 
+    validate do
+        unless Staff::Authenticator.new(object).authenticate(current_password)
+            errors.add(:current_password, :wrong)
+        end
+    end
+
     def save
-        object.password = new_password
-        object.save!
+        if valid?
+            object.password = new_password
+            object.save!
+        end
     end
 end
