b27ee70f79c6413534d12666691719776932eea5
mjksend <mjksendonly@gmail.com>
2018-08-29 18:32:22 +0900

    chapter 19 end

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,18 +1,7 @@
 class Administrator < ActiveRecord::Base
     include EmailHolder
-    # before_validation do
-    #     self.email = normalize_as_email(email)
-    #     self.email_for_index = email.downcase if email
-    # end
+    include PasswordHolder
 
-
-    def password=(raw_password)
-        if raw_password.kind_of?(String)
-            self.hashed_password = BCrypt::Password.create(raw_password)
-        elsif raw_password.nil?
-            self.hashed_password = nil
-        end
-    end
 
     def active?
         !suspended?
