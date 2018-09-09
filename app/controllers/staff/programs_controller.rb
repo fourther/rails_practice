3af2f6b2d8f7df2ec38c3abc0cfe9f37182c5d03
mjksend <mjksendonly@gmail.com>
2018-09-09 13:55:20 +0900

    done 4.2.1

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,4 +1,4 @@
-class Staff::ProgramsController < Staff:Base
+class Staff::ProgramsController < Staff::Base
     def index
         @programs = Program.order(application_start_time: :desc).page(params[:page])
     end
