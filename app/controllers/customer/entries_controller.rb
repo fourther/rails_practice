173051eba267ee9cfd053425ca689bebce9e25bb
mjksend <mjksendonly@gmail.com>
2018-09-12 15:47:26 +0900

    done chapter 6

--- app/controllers/customer/entries_controller.rb
+++ app/controllers/customer/entries_controller.rb
@@ -6,7 +6,17 @@ class Customer::EntriesController < Customer::Base
             flash.notice = 'プログラムに申し込みました'
         when :full
             flash.alert = 'プログラムへの申し込み者数が上限に達しました'
+        when :closed
+            flash.alert = 'プログラムの申し込み期間が終了しました'
         end
         redirect_to [:customer, program]
+    end
+
+    def cancel
+        program = Program.published.find(params[:program_id])
+        entry = program.entries.where(customer_id: current_customer.id).first!
+        entry.update_column(:canceled, true)
+        flash.notice = 'プログラムへの申し込みをキャンセルしました'
+        redirect_to [ :customer, program ]
     end
 end
